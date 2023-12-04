89292e52fdd1b76860bf01326f91a0b424bd29b4
xulihang <xulihanghai@163.com>
2023-12-04 14:05:45 +0800

    update Main

--- src/main/java/com/tonyxlh/searchablePDF4j/Main.java
+++ src/main/java/com/tonyxlh/searchablePDF4j/Main.java
@@ -1,9 +1,12 @@
 package com.tonyxlh.searchablePDF4j;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 
 
 public class Main {
@@ -15,8 +18,15 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        ArrayList<byte[]> images = new ArrayList<>();
-        images.add(byteArray);
-        SearchablePDFCreator.create(images,"F://output.pdf");
+        //ArrayList<byte[]> images = new ArrayList<>();
+        //images.add(byteArray);
+        //SearchablePDFCreator.create(images,"F://output.pdf");
+        String base64 = Base64.getEncoder().encodeToString(byteArray);
+        OCRSpace.key = "your key";
+        OCRResult result = OCRSpace.detect(base64);
+        PDDocument document = new PDDocument();
+        SearchablePDFCreator.addPage(byteArray,result,document,0);
+        document.save(new File("F://output.pdf"));
+        document.close();
     }
 }
